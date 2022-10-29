e0d76fbb8e9785f1d62a81ef80179c551458f383
Tanyu Brandon <82066029+tanyubrandon@users.noreply.github.com>
2022-10-29 19:24:19 +0000

    Adventure story created with print statement and concatenation

---
+++ main.py
@@ -0,0 +1,13 @@
+print("""
+Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!
+""")
+
+name = input("What is your name? ")
+
+enemyName = input("what is your worst enemy's name? ")
+superpower = input("what is your superpower? ")
+live = input("where do you live? ")
+food = input("what is your favourite food? ")
+
+print("\n")
+print("Hello ",name,"! Your ability to" ,superpower, "will make sure you never have to look at",enemyName,  "again. Go eat ",food,"as you walk down the streets of ",live, "and use ",superpower, "for good and not evil!")
